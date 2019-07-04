21bb0602ab72b219d72425957196f1d24a61afc7
czhu88 <christinezhu88@gmail.com>
2019-07-03 22:47:56 -0400

    Question 1 - formatting

--- src/test/scala/MultiplesOfThreeAndFivesSpec.scala
+++ src/test/scala/MultiplesOfThreeAndFivesSpec.scala
@@ -1,13 +1,13 @@
 import org.scalatest.{FreeSpec, Matchers}
 
-class MultiplesOfThreeAndFivesSpec extends FreeSpec with Matchers{
-    "Question One - Multiples of 3 and 5" - {
-        "Solution one should yield 233168 for 999" in {
-            MultiplesOfThreeAndFives.solutionOne(999) shouldBe 233168
-        }
-
-        "Solution two should yield 233168 for 999" in {
-            MultiplesOfThreeAndFives.solutionTwo(999) shouldBe 233168
-        }
+class MultiplesOfThreeAndFivesSpec extends FreeSpec with Matchers {
+  "Question One - Multiples of 3 and 5" - {
+    "Solution one should yield 233168 for 999" in {
+      MultiplesOfThreeAndFives.solutionOne(999) shouldBe 233168
     }
+
+    "Solution two should yield 233168 for 999" in {
+      MultiplesOfThreeAndFives.solutionTwo(999) shouldBe 233168
+    }
+  }
 }
