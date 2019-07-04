56336c098bd992d5ba84842250bdcea7e75ca654
czhu88 <christinezhu88@gmail.com>
2019-07-03 22:34:14 -0400

    Question 1 - added question to the answer

--- src/main/scala/MultiplesOfThreeAndFives.scala
+++ src/main/scala/MultiplesOfThreeAndFives.scala
@@ -1,3 +1,8 @@
+/*
+Q1) If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
 object MultiplesOfThreeAndFives {
   def solutionOne(maxNum: Int): Int = (3 to maxNum).fold(0)((total, num) => if (num % 3 == 0 || num % 5 == 0) total + num else total)
 
