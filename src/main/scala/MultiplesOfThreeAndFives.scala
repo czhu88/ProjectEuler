1c45cff09d61ab4d85d3edda566b81dc5185857d
czhu88 <christinezhu88@gmail.com>
2019-07-03 21:21:29 -0400

    create repo for Project Euler questions with answer to first question

---
+++ src/main/scala/MultiplesOfThreeAndFives.scala
@@ -0,0 +1,12 @@
+object MultiplesOfThreeAndFives {
+  def solutionOne(maxNum: Int): Int = (3 to maxNum).fold(0)((total, num) => if (num % 3 == 0 || num % 5 == 0) total + num else total)
+
+  def solutionTwo(maxNum: Int): Int = calculateMultipleSum(maxNum, 3) + calculateMultipleSum(maxNum, 5) - calculateMultipleSum(maxNum, 15)
+
+  private def calculateMultipleSum(maxNum: Int, multiple: Int) = {
+    val highestMultiple = maxNum / multiple
+    multiple * sumOfSequence(highestMultiple)
+  }
+
+  private def sumOfSequence(sequenceEndNumber: Int): Int = (sequenceEndNumber * (sequenceEndNumber + 1)) / 2
+}
